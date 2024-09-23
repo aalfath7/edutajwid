4d67bccdc998b818264398dbe810f19492dc3200
aalfath7 <abdielhafizhy@gmail.com>
2024-09-23 11:27:01 +0700

    logo edit

--- layouts/home.vue
+++ layouts/home.vue
@@ -9,7 +9,7 @@
         <a href="" class="flex items-center space-x-3 rtl:space-x-reverse">
           <img
             src="/src/logo-edutajwid.png"
-            class="h-10"
+            class="h-10 w-26"
             alt="EduTajwid Logo"
           />
         </a>
